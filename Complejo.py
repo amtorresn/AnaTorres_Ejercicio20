62e8260b07b94d8e4c2ce7dbfbb126b607592e51
Ana Maria Torres <48950516+amtorresn@users.noreply.github.com>
2019-03-28 07:06:40 -0500

    Create Complejo.py

---
+++ Complejo.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class Complejo():
+    def __init__(x, y):
+        self.real = x
+        self.imaginario = y
+        self.norma = np.sqrt(x**2 + y**2)
+        
+    def conjugado(self):
+        
+        
+    def calcula_norma(self):
+        
+    def pow(self):
+        
